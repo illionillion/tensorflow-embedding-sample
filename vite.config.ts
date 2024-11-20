5a65298ebaa3d81c44170b5f7da2f90afe8dc5dd
yuto ichinose <yuto.0526.sano@gmail.com>
2024-11-20 10:56:56 +0900

    build: `rollup-plugin-visualizer`追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // ビルド後に自動でブラウザで開く
+      filename: "bundle-analysis.html", // 出力ファイル名
+    }),
+  ],
   base: process.env.GITHUB_PAGES ? "/tensorflow-embedding-sample/" : "./",
   build: {
     outDir: "docs",
   },
-})
+});
